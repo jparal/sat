3259d68fd6c82b14a1c0761090eb2ff8934917b9
Jan Paral <jparal@gmail.com>
2009-04-25 23:35:48 -0600

    example: create example on how to use Sigmund DF

---
+++ example/rand/sig.cxx
@@ -0,0 +1,27 @@
+/******************************************************************************
+ *   Copyright (C) by Jan Paral and SAT team                                  *
+ *   See docs/license/sat file for copying and redistribution conditions.     *
+ ******************************************************************************/
+/**
+ * @file   sigmund.cxx
+ * @brief  Example of using Sigmund distribution
+ * @author @jparal
+ *
+ * @revision{0.3.0}
+ * @reventry{2008/06, @jparal}
+ * @revmessg{Initial version}
+ */
+
+#include "sat.h"
+#include "stdlib.h"
+
+int main (int argc, char **argv)
+{
+  SigmunRandGen sig;
+  sig.Initialize (2,5000);
+
+  for (int i=0; i<5000000; i++)
+  {
+    printf ("%lf\n", sig.Get ());
+  }
+}
