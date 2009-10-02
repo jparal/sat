39513e1e21553c34ffdf207d2134f52cd2deed67
Jan Paral <jparal@gmail.com>
2009-10-02 10:31:10 -0600

    matlab: add sh5_rdpcles.m function

---
+++ matlab/sh5_rdpcles.m
@@ -0,0 +1,17 @@
+
+function [data] = sh5_rdpcles(fname,tag,itbeg,itstep,itend)
+
+path = sprintf('/Step#%d/%s',itbeg,tag);
+
+tmp = hdf5read(fname,path);
+len = length(tmp);
+nit = (itend-itbeg)/itstep;
+
+data = zeros(nit,len);
+data(1,:) = tmp;
+
+for i=2:nit
+    iter = itbeg + i*itstep;
+    path = sprintf('/Step#%d/%s',iter,tag);
+    data(i,:) = hdf5read(fname,path);
+end
