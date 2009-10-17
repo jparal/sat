200b30f95700340645aced7df1f9010c1d7d388d
Jan Paral <jparal@gmail.com>
2009-10-17 17:52:34 -0600

    matlab: rename sh5_rd1sq to sh5_rdsq and make the function dimension independent

--- matlab/sh5_rdsq.m
+++ matlab/sh5_rdsq.m
@@ -4,13 +4,14 @@
 function dat = sh5_rd1sq(sensor,basename,tag,start,step,stop)
 
 dat = sh5_read(sensor,basename,tag,start);
-nd = length(dat);
+nd = size(dat);
 nt = ((stop-start)/step)+1;
-dat = zeros(nt,nd);
+dat = zeros([nt nd]);
 
 it=0;
 for iter=start:step:stop
     it=it+1;
     % Mag. Field:
-    dat(it,:) = sh5_read('B',basename,tag,iter);
+    tmp = sh5_read('B',basename,tag,iter);
+    dat(it,:) = tmp(:);
 end
