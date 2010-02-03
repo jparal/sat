59c89a5d8646cf8766af863289ea9b7e4dd1fe7a
Jan Paral <jparal@gmail.com>
2010-02-03 08:07:54 -0700

    math: fix the typo in doxygen group naming

--- libs/math/func/stdmath.h
+++ libs/math/func/stdmath.h
@@ -19,45 +19,47 @@
 
 #include <math.h>
 
-/// @addtogroup math_misc
+/// @addtogroup math_func
 /// @{
 
 /// Standard mathematical functions
 namespace Math
 {
-  /// Absolute value
-  template <class T> static T Abs (T v);
-  /// Ceil
-  template <class T> static T Ceil (T v);
-  /// Floor
-  template <class T> static T Floor (T v);
-  /// Sqrt
-  template <class T> static T Sqrt (T v);
-  /// Exp
-  template <class T> static T Exp (T v);
-  /// Ln
-  template <class T> static T Ln (T v);
-  /// Log
-  template <class T> static T Log (T v);
-  /// Log10
-  template <class T> static T Log10 (T v);
-  /// Pow
-  template <class T> static T Pow (T v1, T v2);
-  /// Sin
-  template <class T> static T Sin (T v);
-  /// Cos
-  template <class T> static T Cos (T v);
-  /// Tan
-  template <class T> static T Tan (T v);
-  /// ArcSin
-  template <class T> static T ASin (T v);
-  /// ArcCos
-  template <class T> static T ACos (T v);
-  /// ArcTan
-  template <class T> static T ATan (T v);
-  /// ArcTan2
-  template <class T> static T ATan2 (T v1, T v2);
-};
+
+/// Absolute value
+template <class T> static T Abs (T v);
+/// Ceil
+template <class T> static T Ceil (T v);
+/// Floor
+template <class T> static T Floor (T v);
+/// Sqrt
+template <class T> static T Sqrt (T v);
+/// Exp
+template <class T> static T Exp (T v);
+/// Ln
+template <class T> static T Ln (T v);
+/// Log
+template <class T> static T Log (T v);
+/// Log10
+template <class T> static T Log10 (T v);
+/// Pow
+template <class T> static T Pow (T v1, T v2);
+/// Sin
+template <class T> static T Sin (T v);
+/// Cos
+template <class T> static T Cos (T v);
+/// Tan
+template <class T> static T Tan (T v);
+/// ArcSin
+template <class T> static T ASin (T v);
+/// ArcCos
+template <class T> static T ACos (T v);
+/// ArcTan
+template <class T> static T ATan (T v);
+/// ArcTan2
+template <class T> static T ATan2 (T v1, T v2);
+
+}; // namespace Math
 
 /// @}
 
