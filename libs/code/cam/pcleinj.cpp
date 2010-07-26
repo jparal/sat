82eef5b432e4e6fede3083d032fc39cae2a0d59b
Jan Paral <jparal@gmail.com>
2010-07-26 11:07:30 -0600

    cam: fix particle generation

--- libs/code/cam/pcleinj.cpp
+++ libs/code/cam/pcleinj.cpp
@@ -28,7 +28,16 @@ void CAMCode<B,T,D>::Inject (TSpecie *sp, ScaField &dn, VecField &us)
   {
     Domain<D> dom;
     for (int i=0; i<D; ++i)
-      dom[i] = Range( 0, _meshp.GetCells( i )-1 );
+      dom[i] = Range( 0, _meshp.GetCells( i )-2 );
+
+    // Extend the domain to include the corners of simulation box
+    for (int i=dim+1; i<D; ++i)
+    {
+      if (_layop.GetDecomp().IsLeftBnd( i ))
+	dom[i].Lo()--;
+      if (_layop.GetDecomp().IsRightBnd( i ))
+	dom[i].Hi()++;
+    }
 
     if (_layop.IsOpen( dim ) && _layop.GetDecomp().IsLeftBnd( dim ))
     {
@@ -37,14 +46,29 @@ void CAMCode<B,T,D>::Inject (TSpecie *sp, ScaField &dn, VecField &us)
       DomainIterator<D> it( dom );
       do
       {
-	for (int pp=0; pp<npcles; ++pp)
+	PosVector loc = it.GetLoc();
+	for (int ip=0; ip<npcles; ++ip)
 	{
 	  pcle.vel = v0 + max.Get();
-	  if (-(rnd.Get()) + dt*pcle.vel[dim]*dxi[dim] > 0.)
-	  {
-	    for (int j=0; j<D; ++j)
-	      pcle.pos[j] = (T)it.GetLoc(j) + dt*pcle.vel[j]*dxi[j]*rnd.Get();
 
+	  T r2 = rnd.Get();
+	  bool in = true;
+	  for (int i=0; i<D; ++i)
+	  {
+	    if (!in)
+	      continue;
+
+	    T r1 = (i==dim ? -1. : 1.) * rnd.Get();
+	    T pp = dt*pcle.vel[i]*dxi[i];
+	    T ll = r1 + loc[i] + pp;
+	    if (_pmin[i] < ll && ll < _pmax[i])
+	      pcle.pos[i] = (i==dim ? 0. : 1.) * r1 + loc[i] + pp * r2;
+	    else
+	      in = false;
+	  }
+
+	  if (in)
+	  {
 	    pid = sp->Push( pcle );
 	    sp->Exec( pid, PCLE_CMD_ARRIVED );
 	  }
@@ -60,20 +84,36 @@ void CAMCode<B,T,D>::Inject (TSpecie *sp, ScaField &dn, VecField &us)
       DomainIterator<D> it( dom );
       do
       {
-	for (int pp=0; pp<npcles; ++pp)
+	PosVector loc = it.GetLoc();
+	for (int ip=0; ip<npcles; ++ip)
 	{
 	  pcle.vel = v0 + max.Get();
-	  if (rnd.Get() + dt*pcle.vel[dim]*dxi[dim] < 0.)
-	  {
-	    for (int j=0; j<D; ++j)
-	      pcle.pos[j] = (T)it.GetLoc(j) + dt*pcle.vel[j]*dxi[j]*rnd.Get();
 
+	  T r2 = rnd.Get();
+	  bool in = true;
+	  for (int i=0; i<D; ++i)
+	  {
+	    if (!in)
+	      continue;
+
+	    T r1 = rnd.Get();
+	    T pp = dt*pcle.vel[i]*dxi[i];
+	    T ll = r1 + loc[i] + pp;
+	    if (_pmin[i] < ll && ll < _pmax[i])
+	      pcle.pos[i] = (i==dim ? 0. : 1.) * r1 + loc[i] + pp * r2;
+	    else
+	      in = false;
+	  }
+
+	  if (in)
+	  {
 	    pid = sp->Push( pcle );
 	    sp->Exec( pid, PCLE_CMD_ARRIVED );
 	  }
 	}
       }
       while (it.Next());
+
     }
   }
 }
