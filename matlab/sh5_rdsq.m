85278e811ea10b054e375fe3e2bf37a91d697496
Jan Paral <jparal@gmail.com>
2010-10-07 10:52:47 -0600

    matlab: update sh5_rdsq function

--- matlab/sh5_rdsq.m
+++ matlab/sh5_rdsq.m
@@ -6,14 +6,18 @@ function dat = sh5_rdsq (sensor, basename, tag, start, step, stop)
 %   Example:
 %     by = sh5_rdsq('bump/B','test','B1', 0, 10, 1000);
 
-dat = sh5_read(sensor,basename,tag,start);
-nd = size(dat);
-nt = ((stop-start)/step)+1;
-dat = zeros([nt nd]);
+% dat = sh5_read(sensor,basename,tag,start);
+% nd = size(dat);
+% nt = ((stop-start)/step)+1;
+% dat = zeros([nt nd]);
 
 it=1;
 for iter=start:step:stop
     tmp = sh5_read(sensor,basename,tag,iter);
+    if iter == start
+        ss = size(tmp);
+        dat = zeros([1 ss]);
+    end
     dat(it,:) = tmp(:);
     it=it+1;
 end
