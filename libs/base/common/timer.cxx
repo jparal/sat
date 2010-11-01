90394d74070fe81ed14b010e08c1bd2145ffa98d
Jan Paral <jparal@gmail.com>
2010-11-01 10:53:14 -0600

    base: make timer a simple clean class for wallclock measurements

--- libs/base/common/timer.cxx
+++ libs/base/common/timer.cxx
@@ -15,244 +15,44 @@
 #include "satsysdef.h"
 #include "timer.h"
 
-#ifndef TIMER_MAX_CONCURRENT_TIMERS
-#define TIMER_MAX_CONCURRENT_TIMERS (128)
-#endif
-
-/*
-*************************************************************************
-* The constructor sets the timer name and initializes timer state.      *
-*************************************************************************
-*/
-Timer::Timer(const String& name, const int id)
+Timer::Timer(bool start)
 {
-  m_name = name;
-  m_identifier = id;
-  m_is_running = false;
-  m_is_active = true;
-  m_accesses = 0;
-  m_concurrent_timers = new bool[TIMER_MAX_CONCURRENT_TIMERS];
+  Reset ();
 
-#ifdef HAVE_VAMPIR
-  string::size_type position;
-
-  // parse timers name down to method
-  position = name.find("::");
-  string class_method = name.substr(position+2);
-  position = class_method.find("::");
-  string class_name = class_method.substr(0,position);
-  string method = class_method.substr(position+2);
-
-  // convert strings to char* type
-  char* char_method = new char[method.length()+1];
-  method.copy(char_method,string::npos);
-  char_method[method.length()] = 0;
-
-  char* char_class = new char[class_name.length()+1];
-  class_name.copy(char_class,string::npos);
-  char_class[class_name.length()] = 0;
-
-  VT_symdef(id,char_class,char_method);
-#endif
-
-  /// Create a Tau "timer" to track time.
-  TAU_MAPPING_TIMER_CREATE (tautimer, name, " ", TAU_USER2, "SAMRAI_DEFAULT");
-
-  Clock::Initialize (m_user_start_exclusive);
-  Clock::Initialize (m_user_stop_exclusive);
-  Clock::Initialize (m_system_start_exclusive);
-  Clock::Initialize (m_system_stop_exclusive);
-  Clock::Initialize (m_wallclock_start_exclusive);
-  Clock::Initialize (m_wallclock_stop_exclusive);
-
-  Reset();
+  if (start)
+    Start ();
 }
 
-Timer::~Timer()
+void Timer::Reset ()
 {
-  delete [] m_concurrent_timers;
+  _walltime = 0.;
+  _walltimeStart = 0.;
+  _walltimeStop = 0.;
+  _isrunning = false;
 }
-
-/*
-***************************************************************************
-*                                                                         *
-* Start and stop routines for timers.                                     *
-*                                                                         *
-* For wallclock time: If we have MPI, we use MPI_Wtime to set the         *
-*                     start/stop point.  If we don't have MPI but do      *
-*                     have access to timer utilities in sys/times.h,      *
-*                     we use the time() utility to set the start/start    *
-*                     point.  If we have neither, we set the wallclock    *
-*                     start/stop time to zero.                            *
-*                                                                         *
-* For user time:      If we have access to timer utilities in sys/times.h,*
-*                     we use the times() utility to compute user and      *
-*                     system start/stop point (passing in the tms struct).*
-*                     If we don't have these utilities, we simply set the *
-*                     user and start/stop times to zero.                  *
-*                                                                         *
-* Note that the stop routine increments the elapsed time information.     *
-* Also, the timer manager manipulates the exclusive time information      *
-* the timers when start and stop are called.                              *
-*                                                                         *
-***************************************************************************
-*/
 
 void Timer::Start()
 {
-  m_accesses++;
+  _isrunning = true;
+  Clock::GetWallTime (_walltimeStart);
+}
 
-  if (m_is_active) {
-
-    Clock::Timestamp(m_user_start_total,
-		     m_system_start_total,
-		     m_wallclock_start_total);
-
-    m_is_running = true;
-
-#ifdef HAVE_VAMPIR
-    VT_begin(m_identifier);
-#endif
-
-#ifdef HAVE_TAU
-    /*
-     * Start the TAU timer.  The "tid" is used for threaded systems
-     * so it generally won't apply for us.  The profiler accesses
-     * the timer (given the "tautimer" for this timer object) which
-     * returns "t", and then starts "t".
-     */
-    int tid = RtsLayer::myThread();
-    TAU_MAPPING_PROFILE_TIMER(t, tautimer, tid);
-    TAU_MAPPING_PROFILE_START(t, tid);
-#endif
-
-    /// @@@TODO
-    // TimerManager::getManager()->startTime(this);
-
+void Timer::Update ()
+{
+  if (_isrunning)
+  {
+    Clock::GetWallTime (_walltimeStop);
+    _walltime += _walltimeStop - _walltimeStart;
+    _walltimeStart = _walltimeStop;
   }
 }
+
 void Timer::Stop()
 {
-  if (m_is_active) {
-
-    /// @@@TODO TimerManager::getManager()->stopTime(this);
-
-#ifdef HAVE_VAMPIR
-    VT_end(m_identifier);
-#endif
-
-#ifdef HAVE_TAU
-    TAU_MAPPING_PROFILE_STOP(RtsLayer::myThread());
-#endif
-
-    Clock::Timestamp(m_user_stop_total,
-		     m_system_stop_total,
-		     m_wallclock_stop_total);
-
-    m_is_running = false;
-
-    m_wallclock_total +=
-      double(m_wallclock_stop_total - m_wallclock_start_total);
-    m_user_total += double(m_user_stop_total - m_user_start_total);
-    m_system_total += double(m_system_stop_total - m_system_start_total);
-
-  }
-
-}
-
-void Timer::Reset()
-{
-  m_user_total = 0.;
-  m_system_total = 0.;
-  m_wallclock_total = 0.;
-
-  m_user_exclusive = 0.;
-  m_system_exclusive = 0.;
-  m_wallclock_exclusive = 0.;
-
-  m_max_wallclock = 0.;
-
-  for (int i = 0; i < TIMER_MAX_CONCURRENT_TIMERS; i++) {
-    m_concurrent_timers[i] = false;
+  if (_isrunning)
+  {
+    _isrunning = false;
+    Clock::GetWallTime (_walltimeStop);
+    _walltime += _walltimeStop - _walltimeStart;
   }
 }
-
-/*
-***************************************************************************
-*                                                                         *
-* Compute the load balance efficiency based the wallclock time on each    *
-* processor, using the formula:                                           *
-*                                                                         *
-*      eff = (sum(time summed across processors)/#processors) /           *
-*             max(time across all processors)                             *
-*                                                                         *
-* This formula corresponds to that used to compute load balance           *
-* efficiency based on the processor distribution of the the number of     *
-* cells (i.e. in BalanceUtilities::computeLoadBalanceEfficiency).         *
-*                                                                         *
-***************************************************************************
-*/
-// double Timer::LoadBalanceEfficiency()
-// {
-//   double sum = m_wallclock_total;
-
-//   Mpi::SumReduce (&sum);
-
-//   MaxWallclock();
-//   int nprocs = Mpi::Nodes ();
-
-//   double eff = 100.;
-//   if (m_max_wallclock > 0.) {
-//     eff = 100.*(sum/(double)nprocs)/m_max_wallclock;
-//   }
-//   return eff;
-// }
-
-// void Timer::MaxWallclock()
-// {
-//   double wall = m_wallclock_total;
-//   Mpi::MaxReduce (&wall);
-//   m_max_wallclock = wall;
-// }
-
-// void Timer::putToDatabase(
-// 			  Pointer<Database> db)
-// {
-// #ifdef DEBUG_CHECK_ASSERTIONS
-//   assert(!db.isNull());
-// #endif
-//   db->putInteger("TBOX_TIMER_VERSION",
-// 		 TBOX_TIMER_VERSION);
-
-//   db->putString("m_name",m_name);
-
-//   db->putDouble("m_user_total",m_user_total);
-//   db->putDouble("m_system_total", m_system_total);
-//   db->putDouble("m_wallclock_total", m_wallclock_total);
-
-//   db->putDouble("m_user_exclusive",m_user_exclusive);
-//   db->putDouble("m_system_exclusive", m_system_exclusive);
-//   db->putDouble("m_wallclock_exclusive", m_wallclock_exclusive);
-// }
-
-// void Timer::getFromRestart(
-// 			   Pointer<Database> db)
-// {
-// #ifdef DEBUG_CHECK_ASSERTIONS
-//   assert(!db.isNull());
-// #endif
-//   int ver = db->getInteger("TBOX_TIMER_VERSION");
-//   if (ver != TBOX_TIMER_VERSION) {
-//     TBOX_ERROR("Restart file version different than class version.");
-//   }
-
-//   m_name = db->getString("m_name");
-
-//   m_user_total = db->getDouble("m_user_total");
-//   m_system_total = db->getDouble("m_system_total");
-//   m_wallclock_total = db->getDouble("m_wallclock_total");
-
-//   m_user_exclusive = db->getDouble("m_user_exclusive");
-//   m_system_exclusive = db->getDouble("m_system_exclusive");
-//   m_wallclock_exclusive = db->getDouble("m_wallclock_exclusive");
-// }
--- libs/base/common/timer.h
+++ libs/base/common/timer.h
@@ -16,58 +16,18 @@
 #define __SAT_TIMER_H__
 
 #include "string.h"
-#include "tau.h"
+#include "base/sys/clock.h"
 
 /// @addtogroup base_common
 /// @{
 
-/**
- * Class Timer holds the exclusive and total start, stop, and elapsed time for
- * timers instrumented in SAMRAI.  Total time is simply the time between calls
- * to the start() and stop() functions.  Exclusive time is applicable if there
- * are nested timers called.
- *
- * System and user start and end times are stored as variables of type clock_t,
- * defined in the sys/times.h include file.  A detailed explanation of the
- * structures used to store system and user times is given in the header for
- * the Clock class. This routine simply accesses the functions specified in
- * that class.
- *
- * Wallclock time may be computed by the systems internal clocks which require
- * an object of type clock_t, or by MPI_Wtime() if the code is linked to MPI
- * libraries.
- *
- * In addition to running or not running, a timer may be active or inactive.
- * An inactive timer is one that is created within a program but will never be
- * turned on or off because it is either not specified as active in an input
- * file or it was not explicitly made active by the user.  When a timer is
- * created, it is active by default.
- *
- * Note that the constructor is protected so that timer objects can only be
- * created by the TimerManager class.
- *
- * @see TimerManager
- */
-class TimerManager;
-
-class Timer // : public DescribedClass
+class Timer
 {
-  friend class TimerManager;
 public:
-  /**
-   * The constructor for the Timer class sets timer name string and integer
-   * identifiers, and initializes the timer state.
-   */
-  Timer (const String& name, const int id = -1);
+  Timer (bool start = false);
 
   /// Empty virtual destructor for Timer class.
-  virtual ~Timer ();
-
-  /// Return string name for timer.
-  const String &GetName () const;
-
-  /// Return integer identfier for timer.
-  int GetIdentifier () const;
+  virtual ~Timer () {};
 
   /// Start the timer if active.
   void Start ();
@@ -75,147 +35,66 @@ public:
   /// Stop the timer if active.
   void Stop ();
 
-  /// Start exclusive time.
-  void StartExclusive ();
-
-  /// Stop exclusive time.
-  void StopExclusive ();
+  /// Update the timer if active.
+  void Update ();
 
   /// Reset the state of the timing information.
   void Reset ();
 
-  /// Return total system time (between starts and stops)
-  double GetTotalSystemTime () const;
-
-  /// Return total user time
-  double GetTotalUserTime () const;
-
-  /// Return sum of user and system times.
-  double GetTotalTime() const;
-
   /// Return total wallclock time
-  double GetTotalWallclockTime () const;
-
-  /// Return max wallclock time
-  double GetMaxWallclockTime () const;
-
-  /// Return exclusive system time.
-  double GetExclusiveSystemTime () const;
-
-  /// Return exclusive user time.
-  double GetExclusiveUserTime () const;
-
-  /// Return exclusive wallclock time.
-  double GetExclusiveWallclockTime () const;
-
-  /// Return true if the timer is active; false otherwise.
-  bool IsActive () const;
+  double GetWallclockTime ()
+  { Update (); return _walltime; }
 
   /// Return true if timer is running; false otherwise.
-  bool IsRunning () const;
+  bool IsRunning () const
+  { return _isrunning; }
 
-  /// Mark given integer as id of timer running concurrently with this one.
-  void SetConcurrentTimer (const int id);
+  friend std::ostream& operator<< (std::ostream &os, Timer &time)
+  {
+    double sec = time.GetWallclockTime ();
+    int days = int(sec / (60.*60.*24.));
+    if (days > 0)
+    {
+      os << days << "d:";
+      sec -= double(days * 60*60*24);
+    }
+    int hrs = int(sec / (60.*60.));
+    if (hrs > 0)
+    {
+      os << hrs << "h:";
+      sec -= double(hrs * 60*60);
+    }
+    int mins = int(sec / 60.);
+    if (mins > 0)
+    {
+      os << mins << "m:";
+      sec -= double(mins * 60);
+    }
 
-  /// Return the concurrent timer vector.
-  bool* GetConcurrentTimerVector () const;
+    int prec = os.precision(2);
+    std::ios::fmtflags flags = os.flags ();
 
-  /// Return number of accesses to start()-stop() functions for the timer.
-  int GetNumberAccesses () const;
+    os << std::fixed << sec << "s";
 
-  /// Compute load balance efficiency based on wallclock (non-exclusive) time.
-//   double LoadBalanceEfficiency ();
+    os.precision(prec);
+    os.flags (flags);
+    return os;
+  }
 
-  /// Compute max wallclock time based on total (non-exclusive) time.
-//   void MaxWallclock ();
-
-  /// Write timer data members to database.
-//   virtual void putToDatabase ( Pointer<Database> db );
-
-  /**
-   * Read restarted times from restart database.  When assertion checking is
-   * on, the database pointer must be non-null.
-   */
-//   virtual void getFromRestart (Pointer<Database> db);
-
-protected:
-  /**
-   * Set this timer object to be a inactive.  A timer is set inactive if it is
-   * encountered in the code but it will not be turned on or off during program
-   * execution.  See TimerManager for more information.
-   */
-  void SetInactive ();
+  Timer& operator/= (double v)
+  {
+    _walltime /= v;
+    return *this;
+  }
 
 private:
-  /*
-   * Class name, id, and concurrent timer flag.
-   */
-  String m_name;
-  int m_identifier;
-  bool* m_concurrent_timers;
 
-  bool m_is_running;
-  bool m_is_active;
+  bool _isrunning;
 
-  /*
-   *  Total times (non-exclusive)
-   */
-  double m_user_total;
-  double m_system_total;
-  double m_wallclock_total;
-
-  /*
-   *  Exclusive times
-   */
-  double m_user_exclusive;
-  double m_system_exclusive;
-  double m_wallclock_exclusive;
-
-  /*
-   *  Cross processor times (i.e. determined across processors)
-   */
-  double m_max_wallclock;
-
-  /*
-   *  Timestamps.  User and system times are stored as type clock_t.
-   *  Wallclock time is also stored as clock_t unless the library has
-   * been compiled with MPI.  In this case, the wall time is stored
-   * as type double.
-   */
-  clock_t m_user_start_total;
-  clock_t m_user_stop_total;
-  clock_t m_system_start_total;
-  clock_t m_system_stop_total;
-  clock_t m_user_start_exclusive;
-  clock_t m_user_stop_exclusive;
-  clock_t m_system_start_exclusive;
-  clock_t m_system_stop_exclusive;
-#ifndef HAVE_MPI
-  clock_t m_wallclock_start_total;
-  clock_t m_wallclock_stop_total;
-  clock_t m_wallclock_start_exclusive;
-  clock_t m_wallclock_stop_exclusive;
-#else
-  double m_wallclock_start_total;
-  double m_wallclock_stop_total;
-  double m_wallclock_start_exclusive;
-  double m_wallclock_stop_exclusive;
-#endif
-
-  /*
-   * Counter of number of times timers start/stop
-   * are accessed.
-   */
-  int m_accesses;
-
-  /*
-   * Objects used for performance analysis with TAU.  The "tautimer" mapping
-   * object is a tau timer that is associated with this SAMRAI timer.
-   */
-  TAU_MAPPING_OBJECT(tautimer)
+  double _walltime;
+  double _walltimeStart;
+  double _walltimeStop;
 };
-
-#include "timer.cpp"
 
 /// @}
 
