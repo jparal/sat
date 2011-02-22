e31df76311e03271f18bd8c96343582f21d97eff
Jan Paral <jparal@gmail.com>
2011-02-22 16:38:45 -0700

    matlab: fix assempling HDF5 files

--- matlab/sh5_assemble.m
+++ matlab/sh5_assemble.m
@@ -1,11 +1,44 @@
 function data = sh5_assemble(sensor, basename, tag, iter, split)
 
-for j=0:split(2)-1
-    for i=0:split(1)-1
-        rank = j*split(1) + i;
-        fname = sio_fname(sensor,basename,iter,rank);
-        tmp = hdf5read(fname,tag);
-        ss = size(tmp);
-        data(i*ss(1)+1:(i+1)*ss(1),j*ss(2)+1:(j+1)*ss(2)) = tmp;
+if ~iscell(tag)
+    tag={tag};
+end
+
+for itag=1:length(tag)
+
+    ctag = char(tag(itag));
+    
+    if length(split) == 2
+        for j=0:split(2)-1
+            for i=0:split(1)-1
+                rank = j*split(1) + i;
+                fname = sio_fname(sensor,basename,iter,rank);
+                tmp = hdf5read(fname,ctag);
+                ss = size(tmp);
+                data(i*ss(1)+1:(i+1)*ss(1),j*ss(2)+1:(j+1)*ss(2)) = tmp;
+            end
+        end
+    end
+
+    if length(split) == 3
+        for k=0:split(3)-1
+            for j=0:split(2)-1
+                for i=0:split(1)-1
+                    rank = k*split(2) + j*split(1) + i;
+                    fname = sio_fname(sensor,basename,iter,rank);
+                    tmp = hdf5read(fname,ctag);
+                    ss = size(tmp);
+
+                    data(i*ss(1)+1:(i+1)*ss(1),j*ss(2)+1:(j+1)*ss(2),k*ss(3)+1:(k+1)*ss(3)) = tmp;
+                end
+            end
+        end
+    end
+
+    fname = sio_fname(sensor,basename,iter);
+    if itag == 1
+        hdf5write(fname,ctag,data,'WriteMode','overwrite');
+    else
+        hdf5write(fname,ctag,data,'WriteMode','append');
     end
 end
--- matlab/sh5_assemblesq.m
+++ matlab/sh5_assemblesq.m
@@ -1,6 +1,5 @@
-function data = sh5_assemblesq(sensor, basename, tag, itmin, dit, itmax, split, comps)
+function sh5_assemblesq(sensor, basename, tag, itmin, dit, itmax, split)
 
 for it=itmin:dit:itmax
-    if
-    sh5_assemble(sensor, basename, tag, it, split)
+    sh5_assemble(sensor, basename, tag, it, split);
 end
