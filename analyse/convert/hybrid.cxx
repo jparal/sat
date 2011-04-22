3f58a29b9af06d8e847f6ae60688c97651586926
Jan Paral <jparal@gmail.com>
2011-04-22 12:36:50 -0600

    convert: add thermal energy calculation

--- analyse/convert/hybrid.cxx
+++ analyse/convert/hybrid.cxx
@@ -16,11 +16,14 @@
 
 int main (int argc, char **argv)
 {
-  if (argc != 3)
+  if (argc <= 2)
   {
-    printf ("Usage: %s B0 n0\n\n", argv[0]);
+    printf ("Usage: %s B0 n0 [optional arguments]\n\n", argv[0]);
     printf ("  B0 [nT]     is solar wind magnetic field\n");
     printf ("  n0 [cm^-3]  is solar wind particle density\n\n");
+    printf (" Optional Arguments:\n");
+    printf ("  --vthp NUM  thermal velocity of protons in vA\n");
+    printf ("\n");
     exit (1);
   }
 
@@ -35,11 +38,27 @@ int main (int argc, char **argv)
   bool inv;
   inv = true; // hybrid => SI
   inv = false; // SI => hybrid
-  DBG_INFO ("Mg. Field [nT]:  "<< b0);
+  DBG_INFO ("Mag. Field [nT]: "<< b0);
   DBG_INFO ("Density [cm^-3]: "<< n0);
-  DBG_INFO ("=======================================");
-  DBG_INFO ("Time [1 s]:      "<< si2h.Time (1, inv));
-  DBG_INFO ("Length [1 m]:    "<< si2h.Length (1, inv));
-  DBG_INFO ("Speed [1 m/s]:   "<< si2h.Speed (1, inv));
-  DBG_INFO ("Accel [1 m/s^2]: "<< si2h.Accel (1, inv));
+  DBG_INFO ("======== [ SI to Hyb ] ========");
+  DBG_INFO ("Time:    s     = "<< si2h.Time (1, false) << " Wp^-1");
+  DBG_INFO ("Length:  m     = "<< si2h.Length (1, false) << " Lin");
+  DBG_INFO ("Speed:   m/s   = "<< si2h.Speed (1, false) << " vA");
+  DBG_INFO ("Accel:   m/s^2 = "<< si2h.Accel (1, false));
+  DBG_INFO ("======= [ Hyb to SI ] ========");
+  DBG_INFO ("Time:    1/Wp  = "<< si2h.Time (1, true) << " s");
+  DBG_INFO ("Length:  Lin   = "<< si2h.Length (1, true) << " m");
+  DBG_INFO ("Speed:   vA    = "<< si2h.Speed (1, true) << " m/s");
+  DBG_INFO ("Accel:   Acc   = "<< si2h.Accel (1, true) << " m/s^2");
+  DBG_INFO ("");
+
+  for (int iarg=3; iarg<argc; ++iarg)
+  {
+    if (!strcmp (argv[iarg], "--vthp"))
+    {
+      double vthp = strtod (argv[++iarg], NULL) * si2h.Speed (1, true);
+      DBG_INFO ("T_proton  = "<< vthp*vthp * 1.0440e-08 <<" eV");
+    }
+  }
+
 }
